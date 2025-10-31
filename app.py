761a7aecc0c04f06961052a8082a83a30dc1a45e
ma112020 <ma112020l@outlook.com>
2025-10-31 20:07:23 +0000

    Alterada soma para multiplicação em app.py

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 def add_numbers(a, b):
-    return a + b
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        raise TypeError("Both arguments must be numbers.")
+    return a * b
